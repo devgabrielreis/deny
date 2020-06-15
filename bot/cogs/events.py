fc6ec8cd62acbd2f1a8124aad49ad833dc9ce017
namateeri <63484148+namateeri@users.noreply.github.com>
2020-06-14 21:16:32 -0300

    Update events.py

--- bot/cogs/events.py
+++ bot/cogs/events.py
@@ -11,8 +11,12 @@ class Events(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_guild_channel_delete(self, channel):
-		await self.bot.db.remove_talk_channel(channel.guild.id, channel.id)
-		await self.bot.db.remove_learn_channel(channel.guild.id, channel.id)
+		conn, cur = await self.bot.db.create_connection(channel.guild.id)
+
+		await self.bot.db.remove_talk_channel(conn, cur, channel.id)
+		await self.bot.db.remove_learn_channel(conn, cur, channel.id)
+
+		await self.bot.db.close_connection(conn, cur)
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
@@ -22,21 +26,21 @@ class Events(commands.Cog):
 			return
 		if message.guild is None:
 			return
-		if not self.bot.db.is_guild_in(message.guild.id):
-			await self.bot.db.add_guild(message.guild.id)
-
 		if message.clean_content.startswith((self.bot.command_prefix, '>', '!', '$', '=', '?', '@', '-', '.', ';', '/')):
 			return
 
-		if await self.bot.db.is_learn_channel(message.guild.id, message.channel.id):
-			await self.bot.db.update_chain(message.guild.id, message.clean_content)
+		conn, cur = await self.bot.db.create_connection(message.guild.id)
 
-		if await self.bot.db.is_talk_channel(message.guild.id, message.channel.id):
-			if await self.bot.db.get_guild_saved_messages(message.guild.id) < 50:
-				return
-			if await self.bot.db.calc_probability(message.guild.id, message.channel.id):
-				async with message.channel.typing():
-					await message.channel.send(await self.bot.db.generate(message.guild.id))
+		if await self.bot.db.is_learn_channel(cur, message.channel.id):
+			await self.bot.db.update_chain(conn, cur, message.clean_content)
+
+		if await self.bot.db.is_talk_channel(cur, message.channel.id):
+			if await self.bot.db.get_guild_saved_messages(cur) >= 50:
+				if await self.bot.db.calc_probability(cur, message.channel.id):
+					async with message.channel.typing():
+						await message.channel.send(await self.bot.db.generate(cur))
+
+		await self.bot.db.close_connection(conn, cur)
 
 def setup(bot):
 	bot.add_cog(Events(bot))
